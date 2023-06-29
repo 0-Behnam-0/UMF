bbd4feec054be273ab3cf1173ec25a2c96fb1ef1
0-behnam-0 <mahdi.mohamadiha.znu@gmail.com>
2023-06-29 21:00:54 +0330

    code formated

--- execvp.c
+++ execvp.c
@@ -3,39 +3,42 @@
 #include <string.h>
 #include <unistd.h>
 
-int main() {
-    char command[100];  // Buffer to hold the user input command
+int main()
+{
+    char command[100]; // Buffer to hold the user input command
     printf("Enter a command: ");
     fgets(command, sizeof(command), stdin); // User input
-    
-    command[strcspn(command, "\n")] = '\0';  // Remove newline character from the command
 
-    char * token;  // Split the command into tokens based on spaces
-    const int arg_max = 20;  // Set maximum arg size which is readable only
-    char * args[arg_max];  // Maximum arguments excluding the command itself
+    command[strcspn(command, "\n")] = '\0'; // Remove newline character from the command
+
+    char *token;            // Split the command into tokens based on spaces
+    const int arg_max = 20; // Set maximum arg size which is readable only
+    char *args[arg_max];    // Maximum arguments excluding the command itself
 
     int i = 0;
-    token = strtok(command, " ");  // First element of command parsed into token
-    while (token != NULL && i < arg_max+1) {
+    token = strtok(command, " "); // First element of command parsed into token
+    while (token != NULL && i < arg_max + 1)
+    {
         args[i++] = token;
         token = strtok(NULL, " ");
     }
 
-    args[i] = NULL;  // Set the last element to NULL
+    args[i] = NULL; // Set the last element to NULL
     /*
     Warning: Ignoring this line of code cause the pointer moves into inaccessible memory address,
      and finally 'Bad address' error will be rised!
     */
 
-    printf("args = ");  // printing the user's input
-    for (int j = 0; j < i; j++) {
+    printf("args = "); // printing the user's input
+    for (int j = 0; j < i; j++)
+    {
         printf("%s ", args[j]);
     }
     printf("\n");
 
-    execvp(args[0], args);  // Execute the command
+    execvp(args[0], args); // Execute the command
 
-    perror("execvp");  // Print an error message if execvp fails
+    perror("execvp"); // Print an error message if execvp fails
 
     return 0;
 }
