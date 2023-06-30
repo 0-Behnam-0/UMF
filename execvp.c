69fc3524429b68f5890b6b545fb02425bad24e55
0-behnam-0 <mahdi.mohamadiha.znu@gmail.com>
2023-06-30 15:48:32 +0330

    cd command completed

--- execvp.c
+++ execvp.c
@@ -6,48 +6,59 @@
 int main()
 {
     char command[100]; // Buffer to hold the user input command
-    printf("Enter a command: ");
-    fgets(command, sizeof(command), stdin); // User input
-
-    command[strcspn(command, "\n")] = '\0'; // Remove newline character from the command('\n' will replaced with '\0')
-
-    char *token;            // Split the command into tokens based on spaces
-    const int arg_max = 20; // Set maximum arg size which is readable only
-    char *args[arg_max];    // Maximum arguments excluding the command itself
-
-    int i = 0;
-    token = strtok(command, " "); // First element of command parsed into token
-    while (token != NULL && i < arg_max + 1)
+    do
     {
-        args[i++] = token;
-        token = strtok(NULL, " ");
-    }
+        printf("Enter a command: ");
+        fgets(command, sizeof(command), stdin); // User input
 
-    args[i] = NULL; // Set the last element to NULL
-    /*
-    Warning: Ignoring this line of code cause the pointer moves into inaccessible memory address,
-     and finally 'Bad address' error will be rised!
-    */
+        command[strcspn(command, "\n")] = '\0'; // Remove newline character from the command('\n' will replaced with '\0')
 
-    printf("args = "); // printing the user's input
-    for (int j = 0; j < i; j++)
-    {
-        printf("%s ", args[j]);
-    }
-    printf("\n");
+        char *token;            // Split the command into tokens based on spaces
+        const int arg_max = 20; // Set maximum arg size which is readable only
+        char *args[arg_max];    // Maximum arguments excluding the command itself
 
-    if (strcmp(command, "cd") == 0)
-    {
-        const char * path = args[1];
-        int is_path_valid = chdir(path);  // checking weather the path is valid or not
+        int i = 0;
+        token = strtok(command, " "); // First element of command parsed into token
+        while (token != NULL && i < arg_max + 1)
+        {
+            args[i++] = token;
+            token = strtok(NULL, " ");
+        }
 
-    }
-    else
-    {
-        execvp(command, args); // Execute the command
+        args[i] = NULL; // Set the last element to NULL
+        /*
+        Warning: Ignoring this line of code cause the pointer moves into inaccessible memory address,
+         and finally 'Bad address' error will be rised!
+        */
 
-        perror("execvp"); // Print an error message if execvp fails
-    }
+        printf("args = "); // Printing the user's input
+        for (int j = 0; j < i; j++)
+        {
+            printf("%s ", args[j]);
+        }
+        printf("\n");
+
+        if (strcmp(command, "cd") == 0)
+        {
+            const char *path = args[1];
+            int is_path_valid = chdir(path); // Checking weather the path is valid or not
+
+            if (is_path_valid == 0) 
+            {
+                printf("path = %s\n", path);
+            }
+            else
+            {
+                printf("Failed to change directory\n");
+            }
+        }
+        else
+        {
+            execvp(command, args); // Execute the command
+
+            perror("execvp"); // Print an error message if execvp fails
+        }
+    } while (strcmp(command, "exit") != 0);
 
     return 0;
 }
