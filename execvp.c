d5021df3892cd97ee0e7177c701531e669b6e79e
0-behnam-0 <mahdi.mohamadiha.znu@gmail.com>
2023-06-30 17:30:02 +0330

    current directory is visible now

--- execvp.c
+++ execvp.c
@@ -9,7 +9,20 @@ int main()
     char command[100]; // Buffer to hold the user input command
     do
     {
-        printf("Enter a command: ");
+        char calcwd[1000];                               // Buffer to hold the calculate current working directory(calcwd)
+        char *dir_path = getcwd(calcwd, sizeof(calcwd)); // Get the path and store into 'dir_path'
+        if (dir_path != NULL)                            // Handling path errors
+        {
+            printf("%s $ ", dir_path);
+        }
+        else if (dir_path == NULL)
+        {
+            printf("[NULL] $ ");
+        }
+        else
+        {
+            perror("Error(getcwd)");
+        }
         fgets(command, sizeof(command), stdin); // User input
 
         command[strcspn(command, "\n")] = '\0'; // Remove newline character from the command('\n' will replaced with '\0')
@@ -32,27 +45,18 @@ int main()
          and finally 'Bad address' error will be rised!
         */
 
-        printf("args = "); // Printing the user's input
-        for (int j = 0; j < i; j++)
-        {
-            printf("%s ", args[j]);
-        }
-        printf("\n");
-
         if (strcasecmp(command, "exit") == 0) // Check for exiting input
         {
+            printf("Exiting...\n");
+            sleep(1);
+            printf("\033[2J");
             break;
         }
         if (strcasecmp(command, "cd") == 0) // 'strcasecmp' is the same as 'strcmp' but it ignores the cases
         {
             const char *path = args[1];
             int is_path_valid = chdir(path); // Checking weather the path is valid or not
-
-            if (is_path_valid == 0)
-            {
-                printf("path = %s\n", path);
-            }
-            else
+            if (is_path_valid != 0)
             {
                 printf("Failed to change directory\n");
             }
@@ -63,7 +67,7 @@ int main()
             if (process_id == 0)
             {
                 execvp(command, args); // Execute the command
-                perror("execvp");      // Print an error message if execvp fails
+                perror("Error(exec)"); // Print an error message if execvp fails
                 exit(1);               // Kill the child if error rised
             }
             else if (process_id > 0)
@@ -73,7 +77,7 @@ int main()
             }
             else
             {
-                perror("fork"); // If creating new process via fork falis, error will be printed
+                perror("Error(fork)"); // If creating new process via fork falis, error will be printed
             }
         }
     } while (1); // Keep the program alive
--- test.c
+++ test.c
@@ -1,82 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
 
-int main()
-{
-    char command[100]; // Buffer to hold the user input command
-
-    do
-    {
-        printf("Enter a command: ");
-        fgets(command, sizeof(command), stdin); // User input
-
-        command[strcspn(command, "\n")] = '\0'; // Remove newline character from the command ('\n' will be replaced with '\0')
-
-        char *token;            // Split the command into tokens based on spaces
-        const int arg_max = 20; // Set maximum arg size which is readable only
-        char *args[arg_max];    // Maximum arguments excluding the command itself
-
-        int i = 0;
-        token = strtok(command, " "); // First element of command parsed into token
-        while (token != NULL && i < arg_max)
-        {
-            args[i++] = token;
-            token = strtok(NULL, " ");
-        }
-
-        args[i] = NULL; // Set the last element to NULL
-
-        printf("args = "); // Printing the user's input
-        for (int j = 0; j < i; j++)
-        {
-            printf("%s ", args[j]);
-        }
-        printf("\n");
-
-        if (strcasecmp(args[0], "cd") == 0)
-        {
-            const char *path = args[1];
-            int is_path_valid = chdir(path); // Checking whether the path is valid or not
-
-            if (is_path_valid == 0)
-            {
-                printf("path = %s\n", path);
-            }
-            else
-            {
-                printf("Failed to change directory\n");
-            }
-        }
-        else if (strcasecmp(args[0], "exit") == 0)
-        {
-            break; // Exit the loop when "exit" command is entered
-        }
-        else
-        {
-            pid_t pid = fork(); // Create a new process
-
-            if (pid == 0) // Child process
-            {
-                execvp(args[0], args); // Execute the command
-
-                perror("execvp"); // Print an error message if execvp fails
-                exit(1);          // Terminate the child process if execvp fails
-            }
-            else if (pid > 0) // Parent process
-            {
-                int status;
-                wait(&status); // Wait for the child process to complete
-            }
-            else
-            {
-                perror("fork"); // Print an error message if fork fails
-            }
-        }
-
-    } while (1);
-
+int main() {
+    char cwd[1024]; // Buffer to hold the current working directory
+    
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current working directory: %s\n", cwd);
+    } else {
+        perror("getcwd() error");
+        return 1;
+    }
+    
     return 0;
 }
